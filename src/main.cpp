2ff55f14dda23d98b3410c5364351fdd99b80634
anzipex <anzipex@gmail.com>
2024-09-18 18:30:48 +0300

    removed comments from HeapSort

--- src/main.cpp
+++ src/main.cpp
@@ -142,30 +142,28 @@ void Merge(std::vector<float> &v, uint64_t left, uint64_t mid, uint64_t right) {
 void HeapSort(std::vector<float> &v) {
     size_t n = v.size();
 
-    // Фаза создания кучи (heapify)
     for (size_t i = n / 2; i >= 1; --i) {
-        Sink(v, i, n); // утопить элементы начиная с середины
+        Sink(v, i, n);
     }
 
-    // фаза сортировки (sortdown)
     for (size_t i = 1; i <= n; ++i) {
-        std::swap(v[0], v[n - i]); // поменять корневой элемент с последним
-        Sink(v, 1, n - i); // утопить корневой элемент, чтобы восстановить структуру кучи
+        std::swap(v[0], v[n - i]);
+        Sink(v, 1, n - i);
     }
 }
 
 void Sink(std::vector<float> &v, size_t i, size_t n) {
-    size_t lc = 2 * i; // левый ребенок
+    size_t lc = 2 * i;
     if (lc > n) {
-        return; // нет детей
+        return;
     }
-    size_t rc = lc + 1; // правый ребенок
-    size_t mc = (rc > n) ? lc : (v[lc - 1] > v[rc - 1]) ? lc : rc; // индекс наибольшего ребенка
+    size_t rc = lc + 1;
+    size_t mc = (rc > n) ? lc : (v[lc - 1] > v[rc - 1]) ? lc : rc;
     if (v[i - 1] >= v[mc - 1]) {
-        return; // если элемент больше или равен наибольшему ребенку, завершить
+        return;
     }
-    std::swap(v[i - 1], v[mc - 1]); // поменять местами текущий элемент с наибольшим ребенком
-    Sink(v, mc, n); // рекурсивно утопить следующий элемент
+    std::swap(v[i - 1], v[mc - 1]);
+    Sink(v, mc, n);
 }
 
 int main(int argc, char **argv) {
