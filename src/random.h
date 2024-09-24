8a282cf0763a64ad065ad2a62c8df942cbaee93d
anzipex <anzipex@gmail.com>
2024-09-24 18:57:31 +0300

    increased random range

--- src/random.h
+++ src/random.h
@@ -5,13 +5,13 @@
 int RandInt() {
     std::random_device rd;
     std::mt19937 gen(rd());
-    std::uniform_int_distribution<int> dist(0, 1);
+    std::uniform_int_distribution<int> dist(0, 100);
     return dist(gen);
 }
 
 float RandFloat() {
     std::random_device rd;
     std::mt19937 gen(rd());
-    std::uniform_real_distribution<float> dist(0.0F, 1.0F);
+    std::uniform_real_distribution<float> dist(0.0F, 100.0F);
     return dist(gen);
 }
